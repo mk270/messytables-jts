399844c8fa3e87305289c677637d832201ad0fa2
Martin Keegan <martin@no.ucant.org>
2013-03-30 19:49:15 +0000

    update requirements; fixes #2

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
 	name = "messytables-jts",
 	author = "Martin Keegan",
 	author_email = "martin.keegan@okfn.org",
-	version = "0.2",
+	version = "0.3",
 	license = "Apache Licence v2.0",
 	url = "",
 	download_url = "",
 	description = "Python library for mapping Messytable cell types to strings",
 	py_modules = ["messytables_jts"],
-	install_requires = ["messytables"],
+	install_requires = ["messytables", "json-table-schema"],
 	scripts = ["bin/csv2jts"] 
 )
